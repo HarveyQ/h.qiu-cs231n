63f604a87c4f4ac2c71957f3568390b7c47b10bf
HarveyQ <qiuhuaqi@hotmail.com>
2017-07-11 22:14:47 +0800

    softmax naive loss and gradient completed

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import shuffle
 from past.builtins import xrange
+import math
 
 def softmax_loss_naive(W, X, y, reg):
   """
@@ -24,13 +25,38 @@ def softmax_loss_naive(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
+  num_train = X.shape[0]  # N
+  num_class = W.shape[1]  # C
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
+  # HQ: numerical instability problem is countered by using the c constant    #
   #############################################################################
-  pass
+  for i in xrange(num_train):
+    # calculate loss of this data point
+    xi = X[i]
+    scores = xi.dot(W)  # calculate score of data point Xi
+    const = math.exp(-np.amax(scores))  # constant for numerical stability
+    e_scores = np.exp(scores + const)
+    Li = -math.log(e_scores[y[i]]/(np.sum(e_scores)))
+    loss += Li
+
+    # calculate the gradient of this data point (analytical)
+    idx_yi = np.zeros((1, num_class))
+    idx_yi[0, np.arange(num_class) == y[i]] = 1
+    Myi = np.outer(xi, idx_yi)  # term 1
+    num = np.outer(xi, e_scores)  # term 2: numerator
+    den = np.sum(e_scores)  # term 2: denominator
+    dWi = -Myi + num/den  # assemble gradient
+    dW += dWi  # accumulate gradient
+
+  loss_reg_term = reg * np.sum(np.power(W, 2))  # calculate regularisation term
+  loss = loss/num_train + loss_reg_term  # finalise the loss
+
+  dW = dW/num_train + 2*reg*W  # finalise the gradient
+
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -60,4 +86,3 @@ def softmax_loss_vectorized(W, X, y, reg):
   #############################################################################
 
   return loss, dW
-
