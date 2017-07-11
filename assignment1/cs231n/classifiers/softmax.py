9c951d6974c4d4fff20ae2f527525eb752fa70b9
HarveyQ <qiuhuaqi@hotmail.com>
2017-07-11 22:38:04 +0800

    final commit 11-Jul softmax naive

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -74,12 +74,19 @@ def softmax_loss_vectorized(W, X, y, reg):
   loss = 0.0
   dW = np.zeros_like(W)
 
+  # get some dimensions
+  num_train = X.shape[0]  # N
+  num_class = W.shape[1]  # C
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+
+
+  # loss =
+  # dW =
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
