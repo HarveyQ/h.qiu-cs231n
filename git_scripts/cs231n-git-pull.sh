14d274e3cd72d550432b00da6c334f6cc880ed1c
Huaqi Qiu <huaqi.qiu@outlook.com>
2018-03-10 07:34:12 +0000

    auto push

--- git_scripts/cs231n-git-pull.sh
+++ git_scripts/cs231n-git-pull.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd /Users/HarveyQ/Documents/mlcv/deep_learning/cs231n/cs231n-2017
-
+cd ~/cs231n
 # discard local changes due to PyCharm remote deployment sync
 git checkout -- .
 git pull origin master
